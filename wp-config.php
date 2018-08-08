75f0c55b1d60c5326918a31f6486898829518274
alejandro <alechew@hotmail.com>
2018-08-08 16:22:55 -0500

    Moving the config file to other secure location and pointing the correct wpconfig file.

---
+++ wp-config.php
@@ -0,0 +1,7 @@
+<?php
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Location of your WordPress configuration. */
+require_once(ABSPATH . '../SecureFiles/wp-config.php');
